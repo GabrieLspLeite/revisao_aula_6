c9782f7f9466c0d5e2773c62f07e9cd61a65df40
Gabriel Sabino <gabrielleitealtern@gmail.com>
2024-09-06 17:56:48 -0300

    adicionado menu

---
+++ biblioteca.py
@@ -0,0 +1,21 @@
+def menu():
+    print("1. Adicionar livro")
+    print("2. Listar livros")
+    print("3. Sair")
+
+while True:
+    menu()
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        # Implementação da função adicionar_livro virá em um branch separado
+        print("Funcionalidade em desenvolvimento...")
+    elif opcao == "2":
+        # Implementação da função listar_livros virá em um branch separado
+        print("Funcionalidade em desenvolvimento...")
+    elif opcao == "3":
+        break
+    else:
+        print("Opção inválida.")
+
+# Funções adicionar_livro e listar_livros serão implementadas em branches separados
